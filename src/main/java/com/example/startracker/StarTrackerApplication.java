fe579503016a9aab9ec0e769d4f8a77aece50e09
Ariq Emtenan <emtenan@gmail.com>
2023-11-20 18:43:27 -0500

    Add Spring Boot Application

---
+++ src/main/java/com/example/startracker/StarTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.startracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/startracker/StarTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.startracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
